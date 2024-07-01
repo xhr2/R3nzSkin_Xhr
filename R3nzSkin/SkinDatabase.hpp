4b4a77edcd4a3e64b794e94ed9fca11b8baa687f
xhr <65706755+xhr2@users.noreply.github.com>
2024-07-01 20:33:19 +0800

    Update SkinDatabase.hpp

--- R3nzSkin/SkinDatabase.hpp
+++ R3nzSkin/SkinDatabase.hpp
@@ -90,6 +90,7 @@ public:
 	};
 
 	std::vector<specialSkin> specialSkins{
+		{ FNV("Ahri"), 29, 36, { "Tier1", "Tier2", "Tier3"}},
 		{ FNV("Katarina"), 29, 36, { "Dagger 1", "Dagger 2", "Dagger 3", "Dagger 4", "Dagger 5", "Dagger 6" }},
 		{ FNV("Renekton"), 26, 32, { "Head off", "Head on", "Fins", "Ultimate" } },
 		{ FNV("MissFortune"), 16, 16, { "Scarlet fair", "Zero hour", "Royal arms", "Starswarm" } },
